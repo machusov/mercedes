a0a928916b3618cb53c4ad304d87793a05ad2092
machusov <machusov@hotmail.com>
2023-10-02 13:44:43 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function(){
+  $('.design-slider').slick({
+    dots: false,
+    slidesToShow: 4,
+    variableWidth: true,
+    prevArrow:'<img class="arrow arrow-left" src="/img/arrow-left.png" alt="">',
+    nextArrow:'<img class="arrow arrow-right" src="/img/arrow-right.png" alt="">'
+  })
+});
